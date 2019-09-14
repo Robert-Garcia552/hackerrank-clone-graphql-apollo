af144efc08d2e3dde613c2191d0e364a8c958817
Robert B Garcia <robgarcia552@gmail.com>
2019-09-13 20:24:39 -0400

    rm resolver and create context function for auth purposes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,14 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('../../src/generated/prisma-client')
 
-const resolvers = {
-    Query: {
-        info: () => null,
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-    },
-
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-    },
-}
-
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers,
-    context: { prisma },
+    resolvers: './src/resolvers',
+    context: request => { 
+        return {
+            ...request,
+            prisma,
+        }
+    },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
